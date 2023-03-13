e0edcc9d1e332cbfd0b9eef731c059d02133cdac
noah7000 <111531126+noah7000@users.noreply.github.com>
2023-03-13 09:48:56 -0400

    Delete BoardInterface.java

--- BoardInterface.java
+++
@@ -1,8 +0,0 @@
-public interface BoardInterface {
-    public void setPiece(Piece inPiece, int row, int column);
-    public Piece getPiece(int row, int column);
-    public int getSize();
-    public boolean onBoard(int row, int column);
-    public boolean equals(BoardInterface otherBoard);
-    public String toString();
-}
